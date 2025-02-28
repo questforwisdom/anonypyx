351ecf569d5772a23aa7456d2b6f5ab665c11889
questforwisdom <questforwisdom2@gmail.com>
2025-02-28 22:16:39 +0100

    added query error metric to metric __init__ imports and bumped version

--- anonypyx/metrics/__init__.py
+++ anonypyx/metrics/__init__.py
@@ -1,2 +1,3 @@
 from anonypyx.metrics.preprocessing import preprocess_original_data, preprocess_prediction
 from anonypyx.metrics.pvpc import percentage_of_vulnerable_population
+from anonypyx.metrics.query_error import counting_query_error
